cdb62ad0fa2887621ae691aa29872b8031496ff7
Thomas Silva <thomas@royalsflush.com>
2014-03-25 02:37:13 +0000

    Modified previous problems with the lint of the code

--- prac1/pi.c
+++ prac1/pi.c
@@ -4,28 +4,29 @@
 
 #define NUM_THREADS 6
 
-void* PrintHello(void* threadid) {
+void* printHello(void* threadid) {
     int id = *((int*) threadid);
     printf("\n%d: Hello World!\n", id);
     pthread_exit(NULL);
 }
 
-int main (int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     pthread_t threads[NUM_THREADS];
-    int rc,t;
+    int rc, t;
 
-    for (t=0;t<NUM_THREADS;t++) {
-        printf("Creating thread %d\n",t);
-        rc = pthread_create(&threads[t], NULL, PrintHello, (void*) &t);
+    for (t=0; t<NUM_THREADS; t++) {
+        printf("Creating thread %d\n", t);
+        rc = pthread_create(&threads[t], NULL, printHello, (void*) &t);
         if (rc) {
-            printf("ERROR return code from pthread_create(): %d\n",rc);
+            printf("ERROR return code from pthread_create(): %d\n", rc);
             exit(-1);
         }
     }
 
     /* wait for threads to exit */
-    for(t=0;t<NUM_THREADS;t++) {
-        pthread_join( threads[t], NULL);
+    for (t=0; t<NUM_THREADS; t++) {
+        pthread_join(threads[t], NULL);
     }
+
     return 0;
 }
