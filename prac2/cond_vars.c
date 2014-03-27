e0cf5fd05608e337effc140949b9fe483b876914
Thomas Silva <thomas@royalsflush.com>
2014-03-27 02:07:09 +0000

    Make some alterations to the code and added gitignore

--- prac2/cond_vars.c
+++ prac2/cond_vars.c
@@ -10,40 +10,59 @@ typedef enum { FALSE, TRUE } boolean;
  Fixed to eliminate nonstandard pthread_getthreadid_np()
  Bug fixes */
 
-#define checkResults(string, val) {             \
-if (val) {                                     \
-printf("Failed with %d at %s", val, string); \
-exit(1);                                     \
-}                                              \
-}
-
 #define                 NUMTHREADS     2
 pthread_mutex_t         dataMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t          dataPresentCondition = PTHREAD_COND_INITIALIZER;
 int                     dataPresent=0;
 int                     sharedData=0;
 
+/* Add ons to the original program */
+int producer_active = 1;
+pthread_mutex_t p_active_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void secure_lock(pthread_mutex_t* mutex) {
+    int rc = pthread_mutex_lock(mutex);
+    if (rc) {
+        printf("Failed to secure mutex lock");
+        exit(1);
+    }
+}
+
+void secure_unlock(pthread_mutex_t* mutex) {
+    int rc = pthread_mutex_unlock(mutex);
+    if (rc) {
+        printf("Failed to unlock mutex");
+        exit(1);
+    }
+}
+
 void *theThread(void *threadid) {
     int   rc;
 
     printf("Consumer Thread %.8x: Entered\n",(int)threadid);
-    rc = pthread_mutex_lock(&dataMutex);
-    checkResults("pthread_mutex_lock()\n", rc);
+    secure_lock(&dataMutex);
+    secure_lock(&p_active_mutex);
 
-    while (TRUE) {
+    while (producer_active) {
         /* The boolean dataPresent value is required for safe use of */
         /* condition variables. If no data is present we wait, other */
         /* wise we process immediately.                              */
 
-        while (!dataPresent) {
+        while (!dataPresent && producer_active) {
             printf("Consumer Thread %.8x: Wait for data to be produced\n", (int)threadid);
+            secure_unlock(&p_active_mutex);
             rc = pthread_cond_wait(&dataPresentCondition, &dataMutex);
+
             if (rc) {
                 printf("Consumer Thread %.8x: condwait failed, rc=%d\n", (int)threadid,rc);
                 pthread_mutex_unlock(&dataMutex);
-                exit(1);
+                exit(2);
             }
+
+            secure_lock(&p_active_mutex);
         }
+        secure_unlock(&p_active_mutex);
+
         printf("Consumer Thread %.8x: Found data or Notified, "
                "CONSUME IT while holding lock\n",
                (int)threadid);
@@ -60,8 +79,7 @@ void *theThread(void *threadid) {
         /* Repeat holding the lock. pthread_cond_wait releases it atomically */
     }
     printf("Consumer Thread %.8x: All done\n",(int)threadid);
-    rc = pthread_mutex_unlock(&dataMutex);
-    checkResults("pthread_mutex_unlock()\n", rc);
+    secure_unlock(&dataMutex);
     return NULL;
 }
 
@@ -77,21 +95,24 @@ int main(int argc, char **argv)
     printf("Create/start threads\n");
     for (i=0; i <NUMTHREADS; ++i) {
         rc = pthread_create(&thread[i], NULL, theThread, (void *)i);
-        checkResults("pthread_create()\n", rc);
+        if (rc) {
+            printf("Failed to create thread %d\n", i);
+            exit(3);
+        }
     }
 
     /* The producer loop */
     while (amountOfData--) {
-        printf("Producer: 'Finding' data\n");
+        printf("Producer: 'Finding' data - amount remaining: %d\n",
+               amountOfData);
         sleep(1);
 
-        rc = pthread_mutex_lock(&dataMutex);   /* Protect shared data and flag  */
-        checkResults("pthread_mutex_lock()\n", rc);
+        secure_lock(&dataMutex);   /* Protect shared data and flag  */
         printf("Producer: Make data shared and notify consumer\n");
-        ++sharedData;                          /* Add data                      */
-        dataPresent=1;                         /* Set boolean predicate         */
+        ++sharedData;                          /* Add data  */
+        dataPresent=1;                         /* Set boolean predicate  */
 
-        rc = pthread_cond_signal(&dataPresentCondition); /* wake up a consumer  */
+        rc = pthread_cond_signal(&dataPresentCondition); /* wake up a consumer */
         if (rc) {
             pthread_mutex_unlock(&dataMutex);
             printf("Producer: Failed to wake up consumer, rc=%d\n", rc);
@@ -99,18 +120,25 @@ int main(int argc, char **argv)
         }
 
         printf("Producer: Unlock shared data and flag\n");
-        rc = pthread_mutex_unlock(&dataMutex);
-        checkResults("pthread_mutex_lock()\n",rc);
+        secure_unlock(&dataMutex);
     }
+
+    secure_lock(&p_active_mutex);
+    producer_active = 0;
+    secure_unlock(&p_active_mutex);
 
     printf("Wait for the threads to complete, and release their resources\n");
     for (i=0; i <NUMTHREADS; ++i) {
         rc = pthread_join(thread[i], NULL);
-        checkResults("pthread_join()\n", rc);
+        if (rc) {
+            printf("Failed to join thread %d\n", i);
+            exit(3);
+        }
     }
 
     printf("Clean up\n");
     rc = pthread_mutex_destroy(&dataMutex);
+    rc = pthread_mutex_destroy(&p_active_mutex);
     rc = pthread_cond_destroy(&dataPresentCondition);
     printf("Main completed\n");
     return 0;
